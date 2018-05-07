bdb1830b878fce1fee3fea5dcbf199b99e9de000
tommypet <tommypet@kth.se>
2018-05-08 01:40:57 +0200

    minor

--- WebApplication2/Dtos/CustomerDto.cs
+++ WebApplication2/Dtos/CustomerDto.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using Vidly.Models;
 
 namespace Vidly.Dtos
 {
